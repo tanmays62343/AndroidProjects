bd090ed1258f1a7a9fbfeff3f7c05b283e37d418
tanmays62343 <tanmays62343@gmail.com>
2023-03-22 02:24:21 +0530

    Added SoundEffects

--- TrXFitness/app/src/main/java/com/Trx/ExcerciseActivity.kt
+++ TrXFitness/app/src/main/java/com/Trx/ExcerciseActivity.kt
@@ -1,13 +1,19 @@
 package com.Trx
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.Trx.databinding.ActivityExcerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExcerciseActivity : AppCompatActivity() {
+class ExcerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding : ActivityExcerciseBinding? = null
 
@@ -19,6 +25,9 @@ class ExcerciseActivity : AppCompatActivity() {
 
     private var exerciseList : ArrayList<ExerciseModel>? = null
     private var currentExercise = -1
+
+    private var tts : TextToSpeech? = null
+    private var player : MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +44,21 @@ class ExcerciseActivity : AppCompatActivity() {
         }
         setRestView()
 
+        tts = TextToSpeech(this, this)
+
         exerciseList = Constants.defaultExerciseList()
     }
     private fun setRestView(){
+
+        try{
+            val sound = Uri.parse("android.resource://com.Trx/"+R.raw.app_src_main_res_raw_press_start)
+            player = MediaPlayer.create(applicationContext,sound)
+            player?.isLooping = false
+            player?.start()
+        }catch(e:java.lang.Exception){
+            e.printStackTrace()
+        }
+
         binding?.FrestView?.visibility = View.VISIBLE
         binding?.Title?.visibility = View.VISIBLE
         binding?.TvExerciseName?.visibility  = View.INVISIBLE
@@ -57,11 +78,13 @@ class ExcerciseActivity : AppCompatActivity() {
         binding?.TvExerciseName?.visibility  = View.VISIBLE
         binding?.TvImage?.visibility = View.VISIBLE
         binding?.FexerciseProgressBar?.visibility = View.VISIBLE
-        
+
         if(exerciseTimer != null){
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        speakOut(exerciseList!![currentExercise].getName())
 
         binding?.TvImage?.setImageResource(exerciseList!![currentExercise].getImage())
         binding?.TvExerciseName?.text = exerciseList!![currentExercise].getName()
@@ -115,6 +138,30 @@ class ExcerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+        if(tts != null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        if(player != null){
+            player!!.stop()
+        }
         binding = null
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The Language specified is not supported!")
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!")
+        }
+    }
+
+    private fun speakOut(text : String){
+        tts!!.speak(text,TextToSpeech.QUEUE_ADD,null,"")
     }
 }
