28477ddba575f5e55f5be07b3ff0276d262e851f
tanmays62343 <tanmays62343@gmail.com>
2023-11-28 18:26:11 +0530

    Added Check for internet

--- AHA_Today/app/src/main/java/com/trx/activities/MainActivity.kt
+++ AHA_Today/app/src/main/java/com/trx/activities/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.trx.activities
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.trx.R
@@ -22,37 +25,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if(!isNetworkAvailable()){
+            binding.offline.visibility = View.VISIBLE
+            return
+        }
+
         //Setting our Landing Fragment
         setupFragment(headlinesFragment)
 
         //OnClick Listener for our Bottom Navigation Bar
         binding.bottomNavBar.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.top_headlines ->{
+            when (it.itemId) {
+                R.id.top_headlines -> {
                     setupFragment(headlinesFragment)
                     true
                 }
-                R.id.all_news ->{
+
+                R.id.all_news -> {
                     //TODO :(Create an all news Fragment : note - sir told not to do Bogus parameter)
-                    Toast.makeText(this, "Hello",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this, "Hello",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     true
                 }
-                else ->{
-                    Log.d("BRB","Error in Bottom Nav Bar")
+
+                else -> {
+                    Log.d("BRB", "Error in Bottom Nav Bar")
                     false
                 }
             }
         }
-
     }
 
     //Method to load fragments
-    private fun setupFragment(fragment : Fragment){
+    private fun setupFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
-            replace(binding.fragmentContainer.id,fragment)
+            replace(binding.fragmentContainer.id, fragment)
             commit()
         }
+    }
+
+    //We are checking weather the internet is available or Not (Learn Later)
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
     }
 
 }
