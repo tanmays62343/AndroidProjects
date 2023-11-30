96ac8d9bcbf15119744b92f6a7ef003a689b611d
tanmays62343 <tanmays62343@gmail.com>
2023-11-30 17:17:30 +0530

    Fixing

--- MyChatApp/app/src/main/java/com/trx/models/User.kt
+++
@@ -1,6 +0,0 @@
-package com.trx.models
-
-data class User(
-    var name : String,
-    var email : String
-)
--- MyChatApp/app/src/main/java/com/trx/utils/Consts.kt
+++
@@ -1,7 +0,0 @@
-package com.trx.utils
-
-object Consts {
-
-    const val USERS_COLLECTION = "Users"
-
-}
